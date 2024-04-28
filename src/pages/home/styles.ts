9ab5b252dac18fb43e4ea806d138724e5c15f5aa
Guilherme <guilhermezapas@gmail.com>
2024-04-28 17:38:40 -0300

    Adding google fonts

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -9,12 +9,14 @@ justify-content: center;
 `
 
 export const ProfileCard = styled.div`
+max-width: 54rem;
 padding: 2rem 2.5rem;
 background-color: ${({theme}) => theme["base-profile"]};
 border-radius: 10px;
-margin-top: -50px;
+margin-top: -3.125rem;
 box-shadow: 0px 2px 28px 0px #00000033;
-
+display: flex;
+gap: 32px;
 `
 
 export const Avatar = styled.img`
@@ -23,7 +25,9 @@ max-height: 148px;
 border-radius: 8px;
 `
 
-export const UserInfos = styled.div` `
+export const UserInfos = styled.div`
+
+`
 
 export const Username = styled.h1``
 
